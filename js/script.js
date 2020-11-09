134662ee2746d1f8186231b82ddc2f04e3bef9ab
Brendan walsh <Brendanwalsh@live.com.au>
2020-11-09 16:51:24 +1100

    App runs off canvas functions now + shading

--- js/script.js
+++ js/script.js
@@ -17,14 +17,6 @@ const app = {
         base: {
             c: null,
             ctx: null,
-        },
-        colour: {
-            c: null,
-            ctx: null
-        },
-        normal: {
-            c: null,
-            ctx: null
         }
     },
     scales: [],
@@ -35,23 +27,10 @@ const app = {
     lastScaleColour: null,
     init: function () {
         //engage main draw port
-        app.canvas.base.c = document.getElementById('appCanvas')
-        app.canvas.base.ctx = app.canvas.base.c.getContext('2d');
-        cb = app.canvas.base
-        app.resizeCanvas(cb.c)
-        //engage the colour layer (canvas)
-        app.canvas.colour.c = document.createElement('canvas')
-        app.canvas.colour.ctx = app.canvas.colour.c.getContext('2d')
-        cc = app.canvas.colour
-        cc.c.width = cb.c.width
-        cc.c.height = cb.c.height
-        //engage the normal map layer
-        app.canvas.normal.c = document.createElement('canvas')
-        app.canvas.normal.ctx = app.canvas.normal.c.getContext('2d')
-        cn = app.canvas.normal
-        cn.c.width = cb.c.width
-        cn.c.height = cb.c.height
-
+        app.canvas = document.getElementById('appCanvas')
+        app.ctx = app.canvas.getContext('2d');
+        ctx = app.ctx
+        app.resizeCanvas(app.canvas)
     },
     resizeCanvas: function (canv) {
         styles = window.getComputedStyle(canv)
@@ -59,54 +38,65 @@ const app = {
         canv.height = Number(styles.height.substring(0, styles.height.length - 2))
     },
     drawScreen: function () {
-        //clear all canvi
-        cb.ctx.clearRect(0, 0, cb.c.width, cb.c.height);
-        // cc.ctx.clearRect(0, 0, cc.c.width, cc.c.height);
-        // cn.ctx.clearRect(0, 0, cn.c.width, cn.c.height);
+        ctx.clearRect(0, 0, app.canvas.width, app.canvas.height);
         for (i = app.scales.length - 1; i >= 0; i--) {
             app.drawScale(app.scales[i])
         }
-        // cb.ctx.save();
-        // cb.ctx.globalCompositeOperation = "source-in"
-        // cb.ctx.drawImage(cc.c, 0, 0);
-        // cb.ctx.restore();
     },
     coordToMap: function ([x, y]) {
         x2 = app.z * x * 60 + app.x
-        y2 = app.z * y * 60 + app.y
+        y2 = app.z * y * 50 + app.y
+        if (y % 2 != 0) {
+            x2 += 30
+        }
         return [x2, y2]
     },
     drawScale: function (scale) {
         let c = app.coordToMap([scale.x, scale.y])
-        cb.ctx.beginPath()
-        cb.ctx.moveTo(c[0], c[1])
-        cb.ctx.bezierCurveTo(
+        ctx.beginPath()
+        ctx.moveTo(c[0], c[1])
+        ctx.bezierCurveTo(
             c[0] - 40,
-            c[1] + 30,
+            c[1] + 15,
             c[0] - 40,
-            c[1] + 60,
+            c[1] + 75,
             c[0],
             c[1] + 100)
-        cb.ctx.bezierCurveTo(
-            c[0] + 40,  
-            c[1] + 60,
+        ctx.bezierCurveTo(
             c[0] + 40,
-            c[1] + 30,
+            c[1] + 75,
+            c[0] + 40,
+            c[1] + 15,
             c[0],
             c[1])
-        cb.ctx.closePath()
-        cb.ctx.stroke()
-        cb.ctx.fillStyle = scale.colour
-        cb.ctx.fill()
-        cb.ctx.clearCircle(c[0],c[1],10)
+        ctx.closePath()
+        ctx.fillStyle = scale.colour
+        ctx.fill()
+        ctx.save()
+        ctx.globalCompositeOperation = "destination-out"
+        ctx.beginPath();
+        ctx.arc(c[0], c[1] + 21, 15 * app.z, 0, 2 * Math.PI)
+        ctx.fill()
+        ctx.globalCompositeOperation = "source-atop"
+        ctx.globalAlpha = 0.4;
+
+        ctx.drawImage(
+            img,
+            c[0] - 31,
+            c[1],
+            61 / app.z,
+            100 / app.z)
+        ctx.restore()
     },
     viewportCalc: function (x, y) {
-        x2 = x * (cb.c.width - app.x) / app.z
-        y2 = y * (cb.c.height - app.y) / app.z
+        x2 = x * (app.canvas.width - app.x) / app.z
+        y2 = y * (app.canvas.height - app.y) / app.z
         return [x2, y2]
     },
     update: function () {},
 }
+img = new Image()
+img.src = "./assets/scaleTransp2.png"
 app.init()
 for (i = 0; i < 10; i++) {
     x = new Scale(i, 0, 1, 'green')
@@ -116,4 +106,6 @@ for (i = 0; i < 10; i++) {
     x = new Scale(i, 1, 1, 'green')
     app.scales.push(x)
 }
-app.drawScreen()
+img.onload = function() {
+    app.drawScreen()
+}
